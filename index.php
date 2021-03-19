abf111bd03bae441289ed547cdd7f24d4f24eb5d
SyaMi7 <72727347+SyaMi7@users.noreply.github.com>
2021-03-19 20:52:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,17 +8,19 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="style.css">
+
     <title>Form Belanja</title>
   </head>
   <body>
    
-
+    <legend class="header">FORM BELANJA</legend>
     <div class="container">
+
         <div class="row">
             <div class="col">
-                <form method="POST" action="form_belanja.php"> 
+                <form class="form" method="POST" action="form_belanja.php"> 
 
-                    <legend>Form Belanja</legend>
 
                     <div class="form-group row">
                         <label for="inputEmail3" class="col-sm-2 col-form-label">Customer</label>
@@ -28,8 +30,8 @@
                     </div>
                                 
                     <fieldset class="form-group row">
-                        <legend class="col-form-label col-sm-2 float-sm-left pt-0" for="produk">Produk</legend>
-                        <div class="col-sm-10" id="produk" >
+                        <label class="col-form-label col-sm-2 float-sm-left pt-0" for="produk">Nama Produk</label>
+                        <div class="col-sm-10" id="produk" class="produk">
                             <div class="row">
                                 <div class="form-check col-sm-2">
                                     <input class="form-check-input" type="radio" id="tv" name="produk" value="TV">
@@ -98,7 +100,7 @@
         </div>
     </div>
 
-
+    
     
 
 
@@ -115,5 +117,9 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
     -->
+
+
+
+    <script src="main.js"></script>
   </body>
 </html>
